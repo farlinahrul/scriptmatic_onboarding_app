1db86473beff1d8e20e14dc4f6759192d3809920
farlinahrul <farlynj@gmail.com>
2022-08-24 23:22:55 +0700

    refactor: delete unused class

--- lib/api/api.dart
+++
@@ -1,5 +0,0 @@
-export 'api_exceptions.dart';
-export 'api_provider.dart';
-export 'api_http_overrides.dart';
-
-enum ErrorType { network, client, server, general }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:scriptmatic_onboarding_app/api/api.dart';
 import 'package:scriptmatic_onboarding_app/ui/screens/splash/splash_screen.dart';
 
 void main() async {
-  HttpOverrides.global = ApiHttpOverrides();
-
-  // // Set default orientation to portrait
-  // await SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  //   DeviceOrientation.portraitDown,
-  // ]);
-
   await initializeDateFormatting('in_ID', null)
       .then((_) => runApp(const MyApp()));
 }

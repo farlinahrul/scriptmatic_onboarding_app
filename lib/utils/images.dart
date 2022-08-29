d5614455216bc2c05dfafc86e59ffe49cadc66a1
farlinahrul <farlynj@gmail.com>
2022-08-29 10:01:27 +0700

    fix: blur images

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,14 +1,14 @@
 class AppImagePaths {
   static const String imgLogoScriptmatic =
       'assets/images/logo/logo_scriptmatic.png';
-  static const String imgSplash = 'assets/images/image_splash_screen.png';
-  static const String imgSlider1 = 'assets/images/image_slider_1.png';
-  static const String imgSlider2 = 'assets/images/image_slider_2.png';
-  static const String imgSlider3 = 'assets/images/image_slider_3.png';
-  static const String imgLogin = 'assets/images/image_login.png';
-  static const String imgRegister = 'assets/images/image_register.png';
-  static const String emptyList = 'assets/images/image_empty_list.png';
-  static const String editUser = 'assets/images/image_ic_edit.png';
+  static const String imgSplash = 'assets/images/img_splash_screen.png';
+  static const String imgSlider1 = 'assets/images/img_slider_1.png';
+  static const String imgSlider2 = 'assets/images/img_slider_2.png';
+  static const String imgSlider3 = 'assets/images/img_slider_3.png';
+  static const String imgLogin = 'assets/images/img_login.png';
+  static const String imgRegister = 'assets/images/img_register.png';
+  static const String emptyList = 'assets/images/img_empty_list.png';
+  static const String editUser = 'assets/images/img_ic_edit.png';
 }
 
 class AppIconsPaths {
